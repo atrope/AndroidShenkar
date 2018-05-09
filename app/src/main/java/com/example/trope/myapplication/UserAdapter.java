3105619c291043e2ad3e957661f1d6e536adb681
Alan Trope <atrope@gmail.com>
2018-05-09 20:10:28 +0300

    removing unused dependecy

--- app/src/main/java/com/example/trope/myapplication/UserAdapter.java
+++ app/src/main/java/com/example/trope/myapplication/UserAdapter.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.afollestad.materialdialogs.MaterialDialog;
-
 import java.util.Date;
 import java.util.List;
 
