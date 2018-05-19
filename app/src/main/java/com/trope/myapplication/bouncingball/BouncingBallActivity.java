40efb5529fc09a2ad54e5a1e3186e8cde278be27
Alan Trope <atrope@gmail.com>
2018-05-20 00:29:19 +0300

    Updating Bugs found and simplifying proccess

--- app/src/main/java/com/trope/myapplication/bouncingball/BouncingBallActivity.java
+++ app/src/main/java/com/trope/myapplication/bouncingball/BouncingBallActivity.java
@@ -1,10 +1,8 @@
 package com.trope.myapplication.bouncingball;
 
 import android.graphics.Color;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Display;
 import android.view.View;
 
 import com.trope.myapplication.R;
@@ -13,18 +11,17 @@ import java.util.Random;
 
 public class BouncingBallActivity extends AppCompatActivity{
     protected BouncingBallInside bounceView;
-    protected Display display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.bouncing_balls);
-        display = getWindowManager().getDefaultDisplay();
         bounceView = findViewById(R.id.bounceView);
         bounceView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {onClickScreen();}
+            public void onClick(View v) {
+                onClickScreen(v.getMeasuredWidth(),v.getMeasuredHeight());}
         });
     }
 
@@ -33,17 +30,12 @@ public class BouncingBallActivity extends AppCompatActivity{
         onBackPressed();
         return true;
     }
-    protected void onClickScreen(){
-        Point size = new Point();
-        display.getSize(size);
+    protected void onClickScreen(int maxX, int maxY){
         Random rand = new Random();
-        int xPos = rand.nextInt(size.x)+1;
-        int yPos = rand.nextInt(size.y)+1;
         int sizeBall = rand.nextInt(150)+50;
+        int xPos = rand.nextInt(maxX-(2*sizeBall))+sizeBall;
+        int yPos = rand.nextInt(maxY-(2*sizeBall))+sizeBall;
         int speed = rand.nextInt(30)+1;
-        int r = rand.nextInt(256);
-        int g = rand.nextInt(256);
-        int b = rand.nextInt(256);
-        bounceView.addBall(xPos, yPos, sizeBall,speed,Color.argb(255, r,g,b));
+        bounceView.addBall(xPos, yPos, sizeBall,speed,Color.argb(255, rand.nextInt(256),rand.nextInt(256),rand.nextInt(256)));
     }
 }
