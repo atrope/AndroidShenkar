1ae4f398da1cf1421ea9dada5962bf1ea4285974
Alan Trope <atrope@gmail.com>
2018-05-21 15:19:47 +0300

    adding rect move and changing click to add ball

--- app/src/main/java/com/trope/myapplication/bouncingball/BouncingBallActivity.java
+++ app/src/main/java/com/trope/myapplication/bouncingball/BouncingBallActivity.java
@@ -17,25 +17,11 @@ public class BouncingBallActivity extends AppCompatActivity{
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.bouncing_balls);
-        bounceView = findViewById(R.id.bounceView);
-        bounceView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onClickScreen(v.getMeasuredWidth(),v.getMeasuredHeight());}
-        });
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
-    }
-    protected void onClickScreen(int maxX, int maxY){
-        Random rand = new Random();
-        int sizeBall = rand.nextInt(150)+50;
-        int xPos = rand.nextInt(maxX-(2*sizeBall))+sizeBall;
-        int yPos = rand.nextInt(maxY-(2*sizeBall))+sizeBall;
-        int speed = rand.nextInt(30)+1;
-        bounceView.addBall(xPos, yPos, sizeBall,speed,Color.argb(255, rand.nextInt(256),rand.nextInt(256),rand.nextInt(256)));
     }
 }
--- app/src/main/java/com/trope/myapplication/bouncingball/BouncingBallInside.java
+++ app/src/main/java/com/trope/myapplication/bouncingball/BouncingBallInside.java
@@ -7,16 +7,18 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class BouncingBallInside extends View {
     private List<Ball> balls = new ArrayList<>();
     private Rect rectangle = new Rect();
     private Paint paint= new Paint();
-
+    private boolean isMoving = false;
     public BouncingBallInside(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
@@ -28,17 +30,52 @@ public class BouncingBallInside extends View {
     protected void init(){
         paint = new Paint();
         paint.setColor(Color.BLUE);
+        rectangle.set(0,0,350,200);
     }
-    protected void addBall(int x,int y,int size,int speed,int color){
+    protected void addBall(int x,int y){
         if (balls.size()==10) balls.remove(0);
-        balls.add(new Ball(x,y,size,speed,color));
+        Random rand = new Random();
+        int sizeBall = rand.nextInt(150) + 50;
+        int speed = rand.nextInt(30) + 1;
+
+        balls.add(new Ball(x,y,sizeBall,speed,Color.argb(255, rand.nextInt(256), rand.nextInt(256), rand.nextInt(256))));
     }
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                if(rectangle.contains((int) ev.getX(),(int) ev.getY())){ //If we are clicking the rectangle
+                    isMoving = true;
+                }
+                else { //If its not in rectangle add new ball
+                    addBall((int) ev.getX(), (int) ev.getY());
+                }
+                invalidate();
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                if (isMoving){
+                    int diffX = (int) ev.getX()-rectangle.left;
+                    int diffY = (int) ev.getY()-rectangle.top;
+                    rectangle.set(rectangle.left+diffX,rectangle.top+diffY,rectangle.right+diffX,rectangle.bottom+diffY);
+                }
+                invalidate();
+                break;
+            }
+            case MotionEvent.ACTION_UP:
+                if (isMoving) isMoving = false;
+                break;
+        }
+        return true;
+
+    }
+
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        rectangle.set(canvas.getWidth()/4,canvas.getHeight()-500,canvas.getWidth()-canvas.getWidth()/4,canvas.getHeight());
-        //rectangle.set(0,0,0,0);
+        //rectangle.set(canvas.getWidth()/3,canvas.getHeight()-500,canvas.getWidth()-canvas.getWidth()/2,canvas.getHeight());
+        //
         for(Ball ball : balls){
             ball.move(canvas,rectangle);
             canvas.drawOval(ball.oval,ball.paint);
